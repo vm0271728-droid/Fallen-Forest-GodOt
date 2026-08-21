b2a0c5b6857a130a25846de768dd6018131ef7b6
vm0271728-droid <vm0271728@gmail.com>
2026-08-21 19:58:13 +0300

    Integrate crash logger storage handling

--- scripts/core/crash_logger.gd
+++ scripts/core/crash_logger.gd
@@ -1,17 +1,25 @@
 extends Node
 
-const LOG_DIR := "user://.Fallen-Forest"
-const LOG_FILE := "user://.Fallen-Forest/runtime.log"
+const ANDROID_LOG_DIR := "/storage/emulated/0/.Fallen-Forest"
+const USER_LOG_DIR := "user://.Fallen-Forest"
 
+var log_path: String
 var file: FileAccess
 var started_at := Time.get_datetime_string_from_system()
 
 func _ready() -> void:
-	DirAccess.make_dir_recursive_absolute(ProjectSettings.globalize_path(LOG_DIR))
-	file = FileAccess.open(LOG_FILE, FileAccess.WRITE)
+	log_path = _get_log_path()
+	DirAccess.make_dir_recursive_absolute(log_path.get_base_dir())
+	file = FileAccess.open(log_path, FileAccess.WRITE)
 	write("=== Fallen Forest started ===")
 	write("Time: " + started_at)
 	write("Godot: " + Engine.get_version_info().string)
+	write("Log path: " + log_path)
+
+func _get_log_path() -> String:
+	if OS.get_name() == "Android":
+		return ANDROID_LOG_DIR + "/runtime.log"
+	return USER_LOG_DIR + "/runtime.log"
 
 func write(message: String) -> void:
 	if file == null:
@@ -20,8 +28,8 @@ func write(message: String) -> void:
 	file.flush()
 
 func copy_logs() -> void:
-	if FileAccess.file_exists(LOG_FILE):
-		DisplayServer.clipboard_set(FileAccess.get_file_as_string(LOG_FILE))
+	if FileAccess.file_exists(log_path):
+		DisplayServer.clipboard_set(FileAccess.get_file_as_string(log_path))
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
